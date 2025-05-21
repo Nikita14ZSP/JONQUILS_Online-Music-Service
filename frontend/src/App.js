d96b7197ee3a41465a3366cbd12b6e1c459e541f
NikitaIT <74265606+Nikita14ZSP@users.noreply.github.com>
2025-05-21 17:44:18 +0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <div>
-      <h1>Online Music Service</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
   );
 }
 
