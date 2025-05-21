7c9acec58129bf4312bfe4a8d0cde01b5ec5b166
NikitaIT <74265606+Nikita14ZSP@users.noreply.github.com>
2025-05-21 17:26:53 +0300

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,25 @@
 from fastapi import FastAPI
+from app import models, database
+from app.routes import music, users
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+models.Base.metadata.create_all(bind=database.engine)
+
+app = FastAPI(title="Online Music Service")
+
+# Разрешаем доступ с фронтенда
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Подключаем роуты
+app.include_router(music.router, prefix="/music", tags=["Music"])
+app.include_router(users.router, prefix="/users", tags=["Users"])
 
 @app.get("/")
-def read_root():
-    return {"message": "Welcome to the Online Music Service"}
+def root():
+    return {"message": "Welcome to Online Music Service API"}
