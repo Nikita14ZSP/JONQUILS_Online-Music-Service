82c279aac130490440885b06edd24ff16f5f53da
NikitaIT <74265606+Nikita14ZSP@users.noreply.github.com>
2025-05-21 17:47:58 +0300

    Update SongList.jsx

--- frontend/src/components/SongList.jsx
+++ frontend/src/components/SongList.jsx
@@ -1,1 +1,14 @@
+function SongList({ songs }) {
+  return (
+    <ul>
+      {songs.map((song) => (
+        <li key={song.id}>
+          {song.artist} – {song.title} ({song.genre}, {song.release_year})
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default SongList;
 
