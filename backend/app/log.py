50c04be99d86c52dc4d5ea0f42223b340bf0e1f5
NikitaIT <74265606+Nikita14ZSP@users.noreply.github.com>
2025-05-21 17:29:28 +0300

    Update log.py

--- backend/app/log.py
+++ backend/app/log.py
@@ -1,2 +1,2 @@
-def log_user_action(user_id, action_type, metadata):
-    print(f"User {user_id} did {action_type} with {metadata}")
+def log_user_action(user_id: int, action_type: str, metadata: dict):
+    print(f"{user_id=} {action_type=} {metadata=}")
