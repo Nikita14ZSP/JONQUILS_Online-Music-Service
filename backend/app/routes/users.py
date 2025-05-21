5a5248a92f239791046f32f4f2ce9a9e6bf7b5d4
NikitaIT <74265606+Nikita14ZSP@users.noreply.github.com>
2025-05-21 17:30:56 +0300

    Create users.py

---
+++ backend/app/routes/users.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from app import crud, schemas, database
+
+router = APIRouter()
+
+def get_db():
+    db = database.SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@router.post("/", response_model=schemas.User)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    return crud.create_user(db, user)
