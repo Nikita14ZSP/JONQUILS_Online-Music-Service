dccb384402824f95699002a61c794c767bcbab6e
NikitaIT <74265606+Nikita14ZSP@users.noreply.github.com>
2025-05-21 17:51:30 +0300

    Update SearchBar.jsx

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,1 +1,12 @@
+function SearchBar() {
+  return (
+    <input
+      type="text"
+      placeholder="🔍 Поиск песен, исполнителей..."
+      style={{ padding: "8px", width: "300px", marginBottom: "10px" }}
+    />
+  );
+}
+
+export default SearchBar;
 
