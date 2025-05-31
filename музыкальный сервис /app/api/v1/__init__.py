9cdfd2d015dc73ef79ca56da0bc7a4203cd064b6
NikitaIT <74265606+Nikita14ZSP@users.noreply.github.com>
2025-05-31 20:06:47 +0300

    Delete музыкальный сервис /app directory

--- музыкальный сервис /app/api/v1/__init__.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-
-from app.api.v1.endpoints import tracks, analytics #, artists, albums # Раскомментируем по мере добавления
-
-api_router = APIRouter()
-api_router.include_router(tracks.router, prefix="/tracks", tags=["Tracks"])
-# api_router.include_router(artists.router, prefix="/artists", tags=["Artists"]) # Заглушка
-# api_router.include_router(albums.router, prefix="/albums", tags=["Albums"]) # Заглушка
-api_router.include_router(analytics.router, prefix="/analytics", tags=["Analytics"]) 
--- музыкальный сервис /app/api/v1/endpoints/artists.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter()
-
-# Заглушка для эндпоинтов исполнителей
-@router.get("/")
-async def get_artists():
-    return [{"name": "Artist Example 1"}, {"name": "Artist Example 2"}] 
